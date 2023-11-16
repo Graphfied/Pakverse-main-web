7f1dd614447937db57b5dac2e6c93bc44d4f33a7
abuhurairah127 <abuhurairahMuhammadShafique@gmail.com>
2023-11-16 18:17:26 +0500

    code restructured in pricing component

---
+++ constants/pricing.ts
@@ -0,0 +1,44 @@
+export const pricing = [
+  {
+    _id: 1,
+    name: "Pioneer",
+    price_in_dollars: 1,
+    short_description:
+      "Perfect for early-stage startups or those just beginning their entrepreneurial journey. Get acquainted with the tools and services that will set the foundation for your business's growth.",
+    features: [
+      "Access to basic AI-powered tools to kickstart your business operations.",
+      "Introduction to Web2 with a basic Web2 website template.",
+      "Initial consultancy session to outline your business roadmap.",
+    ],
+    is_growth_package: false,
+  },
+  {
+    _id: 2,
+    name: "Navigator",
+    price_in_dollars: 50,
+    short_description:
+      "Designed for startups ready to take the next leap. With a focus on scaling and expanding, the Navigator package offers advanced tools and support to navigate the challenges of growth.",
+    features: [
+      "Advanced suite of AI-powered tools tailored for scaling businesses.",
+      "Enhanced Web3 website with customizable features and basic blockchain integration.",
+      "Ongoing marketing support and business consultancy to ensure continued growth.",
+      "Early access to ChatGPT 2.0 for beta testing.",
+    ],
+    is_growth_package: true,
+  },
+  {
+    _id: 3,
+    name: "Vanguard",
+    price_in_dollars: "Custom Price",
+    short_description:
+      "For the startups that settle for nothing but the best. The Vanguard package provides an all-encompassing suite of services, tools, and support, ensuring that your business is always at the forefront of innovation. These packages can be further customized based on the specific needs and feedback from potential clients. Let me know if you'd like any changes or additions!",
+    features: [
+      "Full access to all AI tools, including priority access to new features and tools.",
+      "Fully customizable Web3 website with complete blockchain integration.",
+      "Dedicated marketing team support and in-depth business consultancy sessions.",
+      "Priority access to ChatGPT 2.0 upon release and other future tools.",
+      "Exclusive invitations to Pakverse webinars and events.",
+    ],
+    is_growth_package: false,
+  },
+];
